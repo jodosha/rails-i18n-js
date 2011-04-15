7de2340c7a07328740c1be83d6506aa70f513e6f
Luca Guidi <guidi.luca@gmail.com>
2011-04-15 17:52:54 +0200

    Removed commented line

--- lib/rails-i18n-js/rails/i18n_js_generator.rb
+++ lib/rails-i18n-js/rails/i18n_js_generator.rb
@@ -2,7 +2,6 @@ require 'rails/generators'
 
 class I18nJsGenerator < Rails::Generators::Base
   source_root ::File.expand_path('../templates', __FILE__)
-  # argument :layout_name, :type => :string, :default => "application"
 
   def generate_layout
     copy_file "javascripts/locale.js", "public/javascripts/locale.js"
